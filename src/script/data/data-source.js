0a343fce3767769f57e2595f8cf3548bdcec4d42
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-11-08 14:08:07 +0700

    export inport

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,3 +1,4 @@
+import clubs from './clubs.js';
 class DataSource{
   static searchClub(keyword) {
     return new Promise((resolve, reject) => {
@@ -11,3 +12,5 @@ class DataSource{
     });
   }
 }
+
+export default DataSource;
