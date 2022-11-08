97e6825149ec2067ddb3da8ea48ae0f9ef4e40e2
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-11-08 07:09:20 +0700

    ada kemajuan dikit

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -1,11 +1,11 @@
 class AppBar extends HTMLElement {
-  connectedCallback() {
-    this.render();
-  }
- 
-  render() {
-    this.innerHTML = `<h2>Club Finder</h2>`;
-  }
+    connectedCallback(){
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `<h2>Club Finder</h2>`;
+    }
 }
- 
-customElements.define('app-bar', AppBar);
+
+customElements.define("app-bar", AppBar);
